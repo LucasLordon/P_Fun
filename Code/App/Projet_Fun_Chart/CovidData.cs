9ad38ee6b3eafc7d987cf4edcaf8b2e7927e2d7b
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2024-09-13 15:27:14 +0200

    fix(CovidData Class) wrong name
    fixed name of the variable so i can read the csv file

--- Code/App/Projet_Fun_Chart/CovidData.cs
+++ Code/App/Projet_Fun_Chart/CovidData.cs
@@ -8,22 +8,22 @@ namespace Projet_Fun_Chart
 {
     public class CovidData
     {
-        public DateTime Date { get; set; }
-        public string Time { get; set; }
-        public string AbbreviationCantonAndFl { get; set; }
-        public int? NcumulTested { get; set; }
-        public int? NcumulConf { get; set; }
-        public int? NewHosp { get; set; }
-        public int? CurrentHosp { get; set; }
-        public int? CurrentIcu { get; set; }
-        public int? CurrentVent { get; set; }
-        public int? NcumulReleased { get; set; }
-        public int? NcumulDeceased { get; set; }
-        public string Source { get; set; }
-        public int? CurrentIsolated { get; set; }
-        public int? CurrentQuarantined { get; set; }
-        public int? CurrentQuarantinedRiskAreaTravel { get; set; }
-        public int? CurrentQuarantinedTotal { get; set; }
+        public DateTime date { get; set; }
+        public string time { get; set; }
+        public string abbreviation_canton_and_fl { get; set; }
+        public int? ncumul_tested { get; set; }
+        public int? ncumul_conf { get; set; }
+        public int? new_hosp { get; set; }
+        public int? current_hosp { get; set; }
+        public int? current_vent { get; set; }
+        public int? current_icu { get; set; }
+        public int? ncumul_released { get; set; }
+        public int? ncumul_deceased { get; set; }
+        public string source { get; set; }
+        public int? current_isolated { get; set; }
+        public int? current_quarantined { get; set; }
+        public int? current_quarantined_riskareatravel { get; set; }
+        public int? current_quarantined_total { get; set; }
     }
 
 }
