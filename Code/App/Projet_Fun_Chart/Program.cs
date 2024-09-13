8d69415a1cd8420f48cd7d6acf3cc534bf14f7ee
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2024-09-13 15:30:33 +0200

    feat(import Data) new method ImportCsvData
    feated a new methode for importing the data

--- Code/App/Projet_Fun_Chart/Program.cs
+++ Code/App/Projet_Fun_Chart/Program.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using Projet_Fun_Chart;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
