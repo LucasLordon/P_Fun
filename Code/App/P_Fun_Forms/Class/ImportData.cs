09fa4c86f1ddd92df32c3f2fd48f43f1cba03d10
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2024-11-01 21:23:37 +0100

    Revert "feat(P_Fun_Forms) comment all the important code"
    This reverts commit 299518c64957bbe074e29c75788ed4a61a68acab.

--- Code/App/P_Fun_Forms/Class/ImportData.cs
+++ Code/App/P_Fun_Forms/Class/ImportData.cs
@@ -2,40 +2,30 @@
 using CsvHelper;
 using CsvHelper.Configuration;
 using P_Fun_Forms.Class;
-
-// Classe ImportData permettant d'importer des données COVID depuis un fichier CSV
 public class ImportData
 {
-    // Méthode pour importer les données d'un fichier CSV spécifié
     public List<CovidData> ImportCsvData(string csvFilePath)
     {
-        // Création d'une liste pour stocker les enregistrements de données COVID
         var records = new List<CovidData>();
 
         try
         {
-            // Utilisation de StreamReader pour lire le fichier CSV à l'emplacement spécifié
             using (var reader = new StreamReader(csvFilePath))
-            // Initialisation de CsvReader avec une configuration pour les paramètres CSV
             using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
             {
-                Delimiter = ",",              // Spécification du délimiteur (virgule ici)
-                MissingFieldFound = null,      // Ignore les champs manquants
-                HeaderValidated = null         // Ignore les erreurs de validation des en-têtes
+                Delimiter = ",",
+                MissingFieldFound = null,
+                HeaderValidated = null
             }))
             {
-                // Lecture et conversion des enregistrements du fichier CSV en objets CovidData
                 records = new List<CovidData>(csv.GetRecords<CovidData>());
             }
         }
-        // Gestion des erreurs lors de la lecture du fichier CSV
         catch (Exception ex)
         {
-            // Affiche un message d'erreur dans la console si une exception est levée
             Console.WriteLine($"An error occurred while reading the CSV file: {ex.Message}");
         }
 
-        // Retourne la liste des enregistrements de données COVID
         return records;
     }
 }
